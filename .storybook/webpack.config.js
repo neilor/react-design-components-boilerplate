c52bc70883160e8ddfa6b03e5d60a04dfa7e6926
Atif Afzal <atif.afzal@paytm.com>
2018-02-11 21:17:58 +0530

    Add storybook again

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,22 @@
+require('ts-node').register();
+const path = require('path');
+
+const srcPath = path.resolve(__dirname, '../src');
+
+// load the default config generator.
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const webpackConfig = require('../webpack.config');
+
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    loader: require.resolve('awesome-typescript-loader')
+  });
+  // scss
+  config.module.rules.push(webpackConfig.module.loaders[1]);
+  config.resolve.extensions.push('.ts', '.tsx');
+
+  config.resolve.modules = [srcPath, 'node_modules'];
+  return config;
+};
